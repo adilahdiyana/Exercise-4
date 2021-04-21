6f20a68a4aca36874ed47fced4e75dfeaec2732d
adilahdiyana <81138230+adilahdiyana@users.noreply.github.com>
2021-04-21 16:45:01 +0800

    Create Kokuyo.java

---
+++ Kokuyo.java
@@ -0,0 +1,35 @@
+package Exercise4;
+import java.util.Scanner;
+public class Kokuyo extends PencilCase{
+	
+	private String color;
+	private double price, dimensions;
+	
+	Scanner in = new Scanner(System.in);
+	
+	public Kokuyo(String b, String m, double r) {
+		super(b, m, r);
+		System.out.print("Please enter the color\t\t: ");
+		color = in.nextLine();
+		System.out.print("Please enter the price(dollars)\t: " );
+		price = in.nextDouble();
+		System.out.print("Enter the dimensions(inches)\t: ");
+		dimensions = in.nextDouble();
+	}
+	
+	public String getColor() {
+		return color;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	public double getDimensions() {
+		return dimensions;
+	}
+
+		public String toString() {
+			return super.toString() + "\nColor\t\t\t\t: " + color + "\nPrice\t\t\t\t: " + price + " dollars" + "\nDimensions\t\t\t: " + dimensions + "inches";
+	}
+}
